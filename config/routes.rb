5be511597ac013d012302e08595184a23718495c
Sandra <sandra.moutous@gmail.com>
2026-07-14 14:13:04 +0200

    View words and children added with css on application.css

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
   root "home#index"
+  resources :children do
+    resources :words, only: [:new, :create, :edit, :update, :destroy]
+  end
 end
